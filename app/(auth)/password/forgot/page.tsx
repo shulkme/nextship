b8e5b70dc98edd792a91cfdde119dad7cd166de7
shulksteve <shulk.work@gmail.com>
2026-02-12 18:22:28 +0800

    chore/auth: add reset/forgot page

--- app/(auth)/password/forgot/page.tsx
+++ app/(auth)/password/forgot/page.tsx
@@ -1,3 +1,34 @@
+'use client';
+import { RiArrowLeftLine } from '@remixicon/react';
+import { Button, Form, Input } from 'antd';
+
 export default function Page() {
-  return <></>;
+  return (
+    <>
+      <h1 className="font-bold text-3xl text-center mb-4">Forget Password</h1>
+      <p className="text-center mb-12 text-neutral-500">
+        We will sent you a link to reset your password.
+      </p>
+      <Form variant="filled" layout="vertical" size="large">
+        <Form.Item>
+          <Input placeholder="Email" />
+        </Form.Item>
+        <Form.Item>
+          <Button block type="primary">
+            Continue
+          </Button>
+        </Form.Item>
+        <Form.Item>
+          <Button
+            href="/login"
+            block
+            type="text"
+            icon={<RiArrowLeftLine size={18} />}
+          >
+            Back to login
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
 }
--- app/(auth)/password/reset/page.tsx
+++ app/(auth)/password/reset/page.tsx
@@ -1,3 +1,26 @@
+'use client';
+import { Button, Form, Input } from 'antd';
+
 export default function Page() {
-  return <></>;
+  return (
+    <>
+      <h1 className="font-bold text-3xl text-center mb-4">Reset Password</h1>
+      <p className="text-center mb-12 text-neutral-500">
+        Please enter a new password below to change your password.
+      </p>
+      <Form variant="filled" layout="vertical" size="large">
+        <Form.Item>
+          <Input.Password placeholder="New Password" />
+        </Form.Item>
+        <Form.Item>
+          <Input.Password placeholder="Confim Password" />
+        </Form.Item>
+        <Form.Item>
+          <Button block type="primary">
+            Reset
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
 }
