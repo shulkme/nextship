93eb1404dfdc11d6bd14ab88cb57347269d05950
shulksteve <shulk.work@gmail.com>
2026-02-27 11:03:23 +0800

    fix/i18n: fix locale validation

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-import { type routing } from '@/i18n/routing';
+import { type locales } from '@/i18n/config';
 import type messages from '@/locales/en.json';
 import { type NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 declare module 'next-intl' {
   interface AppConfig {
-    Locale: (typeof routing.locales)[number];
+    Locale: (typeof locales)[number];
     Messages: typeof messages;
   }
 }
